0585ab92c68dd1bff38a09c9627fda6a89b30a58
Chikamsohenry <chikamsohenry.3@gmail.com>
2023-02-19 19:21:55 -0800

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,35 +1,34 @@
+#include <stdlib.h>
+#include <time.h>
 #include <stdio.h>
 
 /**
- * main - Prints all possible combinations of three different digits,
- *        in ascending order, separated by a comma followed by a space.
- *
- * Return: Always 0.
+ * main - printing all possible different combinations of two digits.
+ * Description: using the main function
+ * Return: 0
  */
 int main(void)
 {
-	int digit1, digit2, digit3;
-
-	for (digit1 = 0; digit1 < 8; digit1++)
-	{
-		for (digit2 = digit1 + 1; digit2 < 9; digit2++)
-		{
-			for (digit3 = digit2 + 1; digit3 < 10; digit3++)
-			{
-				putchar((digit1 % 10) + '0');
-				putchar((digit2 % 10) + '0');
-				putchar((digit3 % 10) + '0');
-
-				if (digit1 == 7 && digit2 == 8 && digit3 == 9)
-					continue;
-				
-				putchar(',');
-				putchar(' ');
-			}
-		}
-	}
-
-	putchar('\n');
-
-	return (0);
+int m;
+int n;
+int o;
+for (m = 48; m < 56; m++)
+{
+for (n = m + 1; n < 57; n++)
+{
+for (o = n + 1; o < 58; o++)
+{
+putchar(m);
+putchar(n);
+putchar(o);
+if (m != 55 || n != 56 || o != 57)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchar('\n');
+return (0);
 }
