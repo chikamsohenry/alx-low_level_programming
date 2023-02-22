3aa7345114f9708cf718dc020e1a88614085e36d
Chikamsohenry <chikamsohenry.3@gmail.com>
2023-02-22 06:42:01 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,29 @@
+/*
+ * Ohaechesi Christopher Chikamso
+ * C-functions, nested loops
+ * _Putchar
+ * Write a program that prints _putchar, followed by new line.
+ *
+ */
+
+#include <stdio.h>
+
+/**
+ * main - print putchar
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+
+	return (0);
+}
