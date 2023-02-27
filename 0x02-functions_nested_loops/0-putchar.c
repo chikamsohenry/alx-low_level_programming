35d37643c8266c6e18ea146ea3cb34d785df6385
Chikamsohenry <chikamsohenry.3@gmail.com>
2023-02-27 03:19:23 -0800

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,30 @@
+/*
+ * File: 0-putchar.c 
+ *
+ */
+#include <stdio.h>
+ #include "main.h" 
+
+/** 
+* main-print _putchar followed by a new line
+* 
+*Return: always 0
+*/
+int putchar(int ch)
+ {
+	 if(ch == '\n')
+		 printf("_");
+	 printf("p");
+	 printf("u");
+	 printf("t");
+	 printf("c");
+	 printf("h");
+	 printf("a");
+	 printf("r");
+	 printf("\n");
+
+	 return (0);
+ }
+int main(void)
+{
+}
