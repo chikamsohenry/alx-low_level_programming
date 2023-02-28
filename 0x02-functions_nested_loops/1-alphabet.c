982d89f0f690b5071361066994c40cc2db1be80a
Chikamsohenry <chikamsohenry.3@gmail.com>
2023-02-28 05:46:32 -0800

    alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,10 +6,16 @@
  *
  * Return: Always 0 (Success)
  */
-void print_alphabet(void);
+
+void print_alphabet(void)
 {
 	char letter;
-	for (letter = 'a', <= 'z', letter++)
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+
 		putchar(letter);
 	putchar('\n');
 }
+void main(void)
+	{
+	}
