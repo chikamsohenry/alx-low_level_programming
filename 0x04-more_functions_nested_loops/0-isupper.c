bd4af5d5879a548e59e9ad55a26b1b06ec3f3e16
Chikamsohenry <chikamsohenry.3@gmail.com>
2023-03-07 05:09:52 -0800

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,37 +1,14 @@
 #include "main.h"
-<<<<<<< HEAD
-#include "config.h"
 
 /**
- * _isupper - checks is a character is uppercase
- * @c: carrier integer variable
- * Return: 1 if true, 0 if false
- */
-
-int _isupper(int c)
-{
-	if (c >= 65 && c <= 90)
-	{
-		return (1);
-	}
-	return (0);
-=======
-/**
- * _isupper - checks if letters are uppercase.
- * @c: input.
- *
- * Return: 1 if c is uppercase
- * 0 otherwise.
+ * _isupper - a function that checks for uppercase character
+ * @c: input
+ * Return: 1 if c is uppercase, 0 otherwise
  */
 int _isupper(int c)
 {
 	if ((c >= 'A') && (c <= 'Z'))
-	{
 		return (1);
-	}
 	else
-	{
 		return (0);
-	}
->>>>>>> a9ed80c859b3680b73bfbc0a083f46cb584d02d9
 }
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,56 +1,11 @@
-/*
-Main.h
-Written by Matthew Fisher
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
 
-Main.h is included by all source files and includes every header file in the correct order.
-*/
-#pragma once
-
-//
-// Config.h includes a series of #defines used to control compiling options
-//
-#include "Config.h"
-
-//
-// Engine.h includes everything that rarely changes between applications, such as vector/Matrix4 libraries,
-// OpenGL/DirectX graphics devices, software rasterizers, etc.
-//
-#include "Engine.h"
-
-#include "Constants.h"
-
-#include "BaseCodeDLL.h"
-
-#include "AppParameters.h"
-
-#include "Enums.h"
-
-#include "CardDatabase.h"
-#include "GameData.h"
-
-#include "State.h"
-#include "BuyAgenda.h"
-#include "Player.h"
-#include "TestChamber.h"
-#include "MetaTestChamber.h"
-#include "AIUtility.h"
-
-#include "Event.h"
-#include "EventBase.h"
-#include "EventIntrigue.h"
-#include "EventSeaside.h"
-#include "EventAlchemy.h"
-#include "EventProsperity.h"
-#include "EventCustom.h"
-
-#include "CardEffect.h"
-#include "CardEffectBase.h"
-#include "CardEffectIntrigue.h"
-#include "CardEffectSeaside.h"
-#include "CardEffectAlchemy.h"
-#include "CardEffectProsperity.h"
-#include "CardEffectCustom.h"
-
-#include "DominionGame.h"
-
-#include "App.h"
